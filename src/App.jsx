a79f6c35ebb338176e5d5632dd621aa52570c4f3
sergey <serjjiniuss@gmail.com>
2025-10-16 15:19:22 +0400

    MVP

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Header from "./components/Header.jsx";
 import Signup from "./components/Signup.jsx";
 import StateLogin from "./components/StateLogin.jsx";
 import RefLogin from "./components/RefLogin.jsx";
+import CompStateLogin from "./components/CompStateLogin.jsx";
 
 function App() {
     return (
@@ -11,6 +12,7 @@ function App() {
                 <Signup />
                 <StateLogin />
                 <RefLogin />
+                <CompStateLogin />
             </main>
         </>
     );
